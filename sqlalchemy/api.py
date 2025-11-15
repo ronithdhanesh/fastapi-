728604c12d15e4d3d44d69d5b9c6b05de84b70d7
ronithdhanesh <ronithdhanesh@gmail.com>
2025-11-15 11:29:48 +0530

    fastapi and database integration complete

--- sqlalchemy/api.py
+++ sqlalchemy/api.py
@@ -17,6 +17,24 @@ class User(Base):
     email = Column(String, nullable=False, unique=True)
     role = Column(String, nullable=False)
 
+class UserCreate(BaseModel):
+    name:str
+    email:str
+    role:str
+
+class UserResponse(BaseModel):
+    id: int
+    name:str
+    email: str
+    role: str
+
+    class Config:
+        from_attributes = True
+
+
+
+
+
 
 Base.metadata.create_all(bind=engine)
 
@@ -29,4 +47,30 @@ def get_db():
         db.close()
 
 
-get_db()
+@app.get("/")
+def root():
+    return {"message":"this is first endpoint go ahead for CRUD"}\
+
+
+@app.get("/users")
+def get_users(db: Session = Depends((get_db))):
+    users = db.query(User).all()
+    return users
+
+@app.get('/users/{user_id}')
+def get_user(user_id: int, db:Session=Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        return HTTPException(status_code=404, detail="User not found")
+    return user
+
+@app.post("/users")
+def create_user(user: UserCreate,db: Session = Depends(get_db)):
+    if db.query(User).filter(User.email == user.email).first():
+        return HTTPException(status_code=400, detail="User already exists ")
+    db_user = User(**user.dict())
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
